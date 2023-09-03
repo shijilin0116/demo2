9b87337ef1bacc1d89c93ed5a50d172f3640cf4c
Jalon <51255903051@stu.ecnu.edu.cn>
2023-09-03 15:44:35 +0800

    feat: 修改logo的引用方式，从网络链接改为本地图片

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import {Column, Columns} from "@kube-design/components";
 import {FormProvider} from "./context/FormContext";
 import InstallForm from "./Components/InstallForm";
 import ProgressBar from "./Components/ProgressBar";
-
+import logo from './assets/kubekey-logo.svg';
 const App = () => {
   return (
       <div>
           <Columns>
               <Column className={'is-1'}></Column>
               <Column className={'is-2'}>
-                  <img src='https://raw.githubusercontent.com/kubesphere/kubekey/ebfde7ce2320e985a6e02177ee7908ac1dc350f5/docs/img/kubekey-logo.svg' alt='logo' style={{width:'70%',height:'70%',marginTop: '10px'}}></img>
+                  <img src={logo} alt='logo' style={{width:'70%',height:'70%',marginTop: '10px'}}></img>
               </Column>
           </Columns>
           <FormProvider>
